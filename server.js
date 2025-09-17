03145a0e7f3c5da7e13c3f877dd607289cd47d48
Mohamad Abdou <mohamedabdouelbermawy@gmail.com>
2025-09-17 11:29:04 -0400

    Backend

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ app.use(express.json())
 
 let movies = [
    {
+  "id": 1,
   "title": "Inception",
   "director": "Christopher Nolan",
   "year": 2010
 },
 {
+  "id": 2,
   "title": "Superman",
   "director": "James Gunn",
   "year": 2025
@@ -29,6 +31,12 @@ app.post("/movies", (req, res) => {
     const newMovie = { id: Date.now(), title, director, year }
     movies.push(newMovie)
     res.status(201).json(newMovie)
+})
+
+app.delete("/movies/:id", (req, res) => {
+  const id = Number(req.params.id)
+  movies = movies.filter(m => m.id !== id)
+  res.status(204).end()
 })
 
 app.listen(PORT, () => console.log(`server is running on http://localhost:${PORT}`))
